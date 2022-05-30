11798ec4eac7f18e3ec80ab0f2de513fa15b7371
BureBERT <81955957+BureBERT@users.noreply.github.com>
2022-05-30 15:09:04 +0800

    Update run_burebert_ft.sh

--- bugpred/run_burebert_ft.sh
+++ bugpred/run_burebert_ft.sh
@@ -1,5 +1,5 @@
 CUDA_LAUNCH_BLOCKING=1 python run_br_pred.py \
-  --model_name_or_path ./burebert \
+  --model_name_or_path Colorful/BureBERT \
   --train_file ./dataset/priority_train.csv \
   --validation_file ./dataset/priority_valid.csv \
   --test_file ./dataset/priority_test.csv \
@@ -7,9 +7,9 @@ CUDA_LAUNCH_BLOCKING=1 python run_br_pred.py \
   --do_train \
   --do_eval \
   --do_predict \
-  --max_seq_length 512 \
+  --max_seq_length 256 \
   --per_device_train_batch_size 32 \
   --learning_rate 5e-6 \
   --num_train_epochs 10 \
   --save_steps 10000 \
-  --output_dir ./results
+  --output_dir ./results_priority
